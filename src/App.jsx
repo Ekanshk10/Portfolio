da354176bb70b86a52ec591fa920a5dab1cf09d9
Ekanshk10 <156538370+Ekanshk10@users.noreply.github.com>
2025-05-18 19:07:32 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Hero from "./sections/hero.jsx";
+import Hero from "./sections/Hero.jsx";
 import About from "./sections/About.jsx";
 import TechStack from "./sections/TechStack.jsx";
 import Projects from "./sections/Projects.jsx";
